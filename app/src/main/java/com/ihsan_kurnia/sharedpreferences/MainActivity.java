49f395421092b1db69f9ea5443d7dd96144217ea
IhsanKurnia22 <kurniaihsan58@gmail.com>
2021-02-06 23:52:41 +0700

    Initial commit

---
+++ app/src/main/java/com/ihsan_kurnia/sharedpreferences/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ihsan_kurnia.sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
