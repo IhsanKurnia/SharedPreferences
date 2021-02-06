9db22be5c235b6e043a43744d4ada5117674aa00
IhsanKurnia22 <kurniaihsan58@gmail.com>
2021-02-07 00:32:35 +0700

    Halaman LoginActivity testing versi 01 Tambah splashScreenActivity versi 01

---
+++ app/src/main/java/com/ihsan_kurnia/sharedpreferences/splashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.ihsan_kurnia.sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        
+    }
+}
